fc3098fc8932abe00e54a9cc5e664cdd79a50e28
dmoquia <dennis.moquia@gmail.com>
2022-12-16 16:16:50 +0800

    remove console log

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ function getQuote() {
   fetch(url, { cache: "no-cache" })
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       let advice = `${data.slip.advice}`;
       let adviceNum = `${data.slip.id}`;
       document.querySelector("#advicenum").innerHTML = "Advice # " + adviceNum;
